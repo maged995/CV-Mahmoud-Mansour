a692506cdef22972695ccab1f657c76d1f1e9a09
maged995 <melhabrouk10@gmail.com>
2021-11-28 23:47:18 +0200

    -v 1.0.0

---
+++ mailer/form_setting.php
@@ -0,0 +1,7 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$to = 'me@mahmoudmansour.net'; // Enter Your E-mail
+	$from = 'website@mahmoudmansour.com'; // Admin e-mail
+	$fromName = 'CV '; // Your company name
+	$charset = 'UTF-8';
